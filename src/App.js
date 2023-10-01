6aa471efea9abb0c2259c670a47b9629037545c7
SamiraMiss <samirarostami71@gmail.com>
2023-10-02 00:13:47 +0330

    Worked on new edit page.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,41 @@
-import { Routes, Route, Link } from "react-router-dom";
+import React, { useEffect, useState } from "react";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Link,
+  Switch,
+} from "react-router-dom";
 import Table from "./components/Table";
 import Form from "./components/EmployeeInfo";
 import ErrorPage from "./components/ErrorPage";
-import EmployeePage from "./components/NewPage";
+import EditEmployee from "./components/EditEmployee";
+import axios from "axios";
 
 function App() {
+  const [employees, setEmployees] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get("./MockData/sample.json")
+      .then((response) => setEmployees(response.data));
+  }, []);
+
+  const onUpdateEmployee = (formData) => {
+    // Find the index of the employee to update in the employees array
+    const employeeIndex = employees.findIndex((emp) => emp.id === formData.id);
+
+    if (employeeIndex !== -1) {
+      // Create a copy of the employees array
+      const updatedEmployees = [...employees];
+
+      // Update the employee with the new data
+      updatedEmployees[employeeIndex] = formData;
+
+      // Update the state with the new array
+      setEmployees(updatedEmployees);
+    }
+  };
   return (
     <div className="app">
       <>
@@ -13,12 +44,23 @@ function App() {
         <Link to="/form"> Form </Link>
         <br />
         <Link to="/employee"> Employees </Link>
-        <Routes>
-          <Route path="/" element={<Table />} />
-          <Route path="/form" element={<Form />} />
-          <Route path="*" element={<ErrorPage />} />
-          <Route path="/employee" element={<EmployeePage />} />
-        </Routes>
+        <Router>
+          <Switch>
+            <Route path="/edit/:id">
+              <EditEmployee
+                employees={employees}
+                onUpdateEmployee={onUpdateEmployee}
+              />
+            </Route>
+            {/* Other routes */}
+            <Routes>
+              <Route path="/" element={<Table />} />
+              <Route path="/form" element={<Form />} />
+              <Route path="*" element={<ErrorPage />} />
+              <Route path="/employee" element={<EditEmployee />} />
+            </Routes>
+          </Switch>
+        </Router>
       </>
     </div>
   );
---
+++ src/components/EditEmployee/index.js
@@ -0,0 +1,240 @@
+import React, { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+
+import Box from "@mui/material/Box";
+import TextField from "@mui/material/TextField";
+
+import InputAdornment from "@mui/material/InputAdornment";
+import { CircularProgress, createTheme, ThemeProvider } from "@mui/material";
+import Stack from "@mui/material/Stack";
+import { styled } from "@mui/material/styles";
+import Button from "@mui/material/Button";
+import axios from "axios";
+import Snackbar from "@mui/material/Snackbar";
+import Slide from "@mui/material/Slide";
+
+const theme = createTheme({
+  typography: {
+    fontSize: 22,
+  },
+});
+const CustomButton = styled(Button)({
+  boxShadow: "none",
+  textTransform: "none",
+  fontSize: 16,
+  padding: "6px 12px",
+  border: "1px solid",
+  lineHeight: 1.5,
+  backgroundColor: "#0063cc",
+  borderColor: "#0063cc",
+  "&:hover": {
+    backgroundColor: "#0069d9",
+    borderColor: "#0062cc",
+    boxShadow: "none",
+  },
+  "&:active": {
+    boxShadow: "none",
+    backgroundColor: "#0062cc",
+    borderColor: "#005cbf",
+  },
+  "&:focus": {
+    boxShadow: "0 0 0 0.2rem rgba(0,123,255,.5)",
+  },
+});
+function TransitionLeft(props) {
+  return <Slide {...props} direction="left" />;
+}
+function EditEmployee({ onUpdateEmployee }) {
+  const { id } = useParams();
+  const [formData, setFormData] = React.useState({
+    id: 0,
+    name: "",
+    age: "",
+    department: "",
+  });
+
+  const [formErrors, setFormErrors] = React.useState({
+    id: false,
+    name: "",
+    age: "",
+    department: "",
+  });
+  const [isLoading, setIsLoading] = React.useState(false);
+  const [snackbarOpen, setSnackbarOpen] = React.useState(false);
+  const [snackbarMessage, setSnackbarMessage] = React.useState("");
+  const [transition, setTransition] = React.useState(undefined);
+
+  useEffect(() => {
+    // Fetch employee data based on the id parameter using Axios
+    axios.get(`../MockData/sample.json`).then((response) => {
+      const employees = response.data;
+      const employee = employees.find((emp) => emp.id === parseInt(id, 10));
+
+      if (employee) {
+        setFormData({
+          id: employee.id,
+          name: employee.name,
+          age: employee.age,
+          department: employee.department,
+        });
+      } else {
+        showSnackbar(`Employee with ${id} not defined!!`);
+        // Handle the case where the employee with the given ID doesn't exist
+        // You can show an error message or redirect the user, for example.
+      }
+    });
+  }, [id]);
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({
+      ...formData,
+      [name]: value,
+    });
+    const validationResult = validateField(name, value);
+    setFormErrors({
+      ...formErrors,
+      [name]: validationResult,
+    });
+  };
+  const showSnackbar = (message) => {
+    setSnackbarMessage(message);
+    setSnackbarOpen(true);
+  };
+  const checkValidation = () => {
+    const errors = { ...formErrors };
+
+    errors.id =
+      !formData.id || !formData.id.trim() ? "user Id is required" : "";
+    errors.name = !formData.name.trim() ? "name is required" : "";
+
+    setFormErrors(errors);
+  };
+  const validateField = (name, value) => {
+    switch (name) {
+      case "userId":
+        checkValidation(value);
+        break;
+      case "title":
+        checkValidation(value);
+        break;
+      case "body":
+        checkValidation(value);
+        break;
+      default:
+        return true;
+    }
+  };
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    //Validation logic
+    checkValidation();
+    setIsLoading(true);
+    // Update the employee in your JSON file or database
+    onUpdateEmployee(formData);
+
+    // Redirect the user or show a success message
+  };
+  const handleClick = (Transition) => () => {
+    setTransition(() => Transition);
+  };
+  return (
+    <ThemeProvider theme={theme}>
+      <Box
+        component="form"
+        sx={{
+          "& > :not(style)": { width: "35rem", m: 4 },
+          justifyContent: "center",
+        }}
+        noValidate
+        onSubmit={handleSubmit}
+        autoComplete="off"
+      >
+        <div>
+          <TextField
+            fullWidth
+            helperText={formErrors.name ? "" : "Please enter your Name"}
+            error={Boolean(formErrors.name)}
+            id="outlined-basic"
+            label="Name"
+            name="name"
+            value={formData.name}
+            onChange={handleChange}
+            required
+          />
+        </div>
+        <div>
+          <TextField
+            helperText={
+              formErrors.department ? "" : "Please enter your department"
+            }
+            id="outlined-start-adornment"
+            label="department"
+            fullWidth
+            InputProps={{
+              startAdornment: (
+                <InputAdornment position="start">🤞</InputAdornment>
+              ),
+            }}
+            name="department"
+            value={formData.department}
+            onChange={handleChange}
+          />
+        </div>
+
+        <div>
+          <TextField
+            helperText={formErrors.id ? formErrors.id : "Please enter your ID"}
+            error={Boolean(formErrors.id)}
+            id="demo-helper-txt-misaligned"
+            label="id"
+            fullWidth
+            type="number"
+            name="id"
+            value={formData.id}
+            onChange={handleChange}
+            required
+          />
+          <TextField
+            helperText={
+              formErrors.age ? formErrors.age : "Please enter your age"
+            }
+            error={Boolean(formErrors.age)}
+            id="demo-helper-txt-misaligned"
+            label="age"
+            fullWidth
+            type="number"
+            name="age"
+            value={formData.age}
+            onChange={handleChange}
+            required
+          />
+        </div>
+
+        <Stack spacing={2} direction="row">
+          {isLoading ? (
+            <CircularProgress size={34}></CircularProgress>
+          ) : (
+            <CustomButton
+              type="submit"
+              variant="outlined"
+              onClick={handleClick(TransitionLeft)}
+            >
+              Edit Employee
+            </CustomButton>
+          )}
+        </Stack>
+        <Snackbar
+          TransitionComponent={transition}
+          key={transition ? transition.name : ""}
+          open={snackbarOpen}
+          autoHideDuration={5000}
+          onClose={() => setSnackbarOpen(false)}
+          message={snackbarMessage}
+        ></Snackbar>
+      </Box>
+    </ThemeProvider>
+  );
+}
+
+export default EditEmployee;
