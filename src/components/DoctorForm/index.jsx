91a14eee2cd9ef2b85569d1427182bf16db5954d
SamiraMiss <samirarostami71@gmail.com>
2023-12-11 20:21:20 +0330

    Deleted comments.

--- src/components/DoctorForm/index.jsx
+++ src/components/DoctorForm/index.jsx
@@ -2,8 +2,9 @@ import React, { useState } from "react";
 import Box from "@mui/material/Box";
 import TextField from "@mui/material/TextField";
 
-import { CircularProgress, useTheme, ThemeProvider } from "@mui/material";
+import CircularProgress from "@mui/material/CircularProgress";
 import Stack from "@mui/material/Stack";
+import { useTheme, ThemeProvider } from "@mui/material";
 import { styled } from "@mui/material/styles";
 import Button from "@mui/material/Button";
 
@@ -62,8 +63,6 @@ export default function DoctorForm(props) {
 
   const [isLoading, setIsLoading] = useState(false);
 
-  const [transition, setTransition] = useState(undefined);
-
   /*update state*/
   const handleChange = (e) => {
     const { name, value } = e.target;
@@ -77,18 +76,13 @@ export default function DoctorForm(props) {
       [name]: validationResult,
     });
   };
-
+  //****************************************** */
   const handleSubmit = (e) => {
     e.preventDefault();
     props.submitHandler(props.formData);
   };
-  // const showSnackbar = (message) => {
-  //   setSnackbarMessage(message);
-  //   setSnackbarOpen(true);
-  // };
-
   const handleClick = (Transition) => () => {
-    setTransition(() => Transition);
+    props.setTransition(() => Transition);
   };
 
   return (
@@ -106,11 +100,7 @@ export default function DoctorForm(props) {
         <div>
           <CssTextField
             fullWidth
-            helperText={
-              props.formErrors && props.formErrors.name
-                ? ""
-                : "Please enter your Name"
-            }
+            helperText={props.formErrors.name ? "" : "Please enter your Name"}
             error={Boolean(props.formErrors.name)}
             id="outlined-basic"
             label="Name"
@@ -169,13 +159,13 @@ export default function DoctorForm(props) {
               variant="outlined"
               onClick={handleClick(TransitionLeft)}
             >
-              Create Doctor
+              {props.buttonText}
             </CustomButton>
           )}
         </Stack>
         <Snackbar
-          TransitionComponent={transition}
-          key={transition ? transition.name : ""}
+          TransitionComponent={props.transition}
+          key={props.transition ? props.transition.name : ""}
           open={props.snackbarOpen}
           autoHideDuration={5000}
           onClose={() => props.setSnackbarOpen(false)}
@@ -185,216 +175,3 @@ export default function DoctorForm(props) {
     </ThemeProvider>
   );
 }
-
-// import * as React from "react";
-// import Box from "@mui/material/Box";
-// import TextField from "@mui/material/TextField";
-
-// import { CircularProgress, useTheme, ThemeProvider } from "@mui/material";
-// import Stack from "@mui/material/Stack";
-// import { styled } from "@mui/material/styles";
-// import Button from "@mui/material/Button";
-// import axios from "axios";
-// import Snackbar from "@mui/material/Snackbar";
-// import Slide from "@mui/material/Slide";
-// import { useNavigate } from "react-router";
-
-// const CustomButton = styled(Button)({
-//   boxShadow: "none",
-//   textTransform: "none",
-//   fontSize: 14,
-//   color: "#fff",
-//   padding: "6px 12px",
-//   border: "1px solid",
-//   lineHeight: 1.5,
-//   backgroundColor: "#0063cc",
-//   borderColor: "#0063cc",
-//   "&:hover": {
-//     backgroundColor: "#0069d9",
-//     borderColor: "#0062cc",
-//     boxShadow: "none",
-//   },
-//   "&:active": {
-//     boxShadow: "none",
-//     backgroundColor: "#0062cc",
-//     borderColor: "#005cbf",
-//   },
-//   "&:focus": {
-//     boxShadow: "0 0 0 0.2rem rgba(0,123,255,.5)",
-//   },
-// });
-
-// function TransitionLeft(props) {
-//   return <Slide {...props} direction="left" />;
-// }
-// export default function DoctorInfo(props) {
-//   const theme = useTheme();
-//   const [formData, setFormData] = React.useState({
-//     id: 0,
-//     name: "",
-//     specialty: "",
-//     location: "",
-//     phone: "",
-//     email: "",
-//   });
-
-//   const [formErrors, setFormErrors] = React.useState({
-//     id: "",
-//     name: "",
-//     specialty: "",
-//     location: "",
-//     phone: "",
-//     email: "",
-//   });
-
-//   const [isLoading, setIsLoading] = React.useState(false);
-
-//   const [snackbarOpen, setSnackbarOpen] = React.useState(false);
-
-//   const [transition, setTransition] = React.useState(undefined);
-
-//   const handleClick = (Transition) => () => {
-//     setTransition(() => Transition);
-//   };
-
-//   /*update state*/
-//   const handleChange = (e) => {
-//     const { name, value } = e.target;
-//     setFormData({
-//       ...formData,
-//       [name]: value,
-//     });
-//     const validationResult = props.validateField(name, value);
-//     setFormErrors({
-//       ...formErrors,
-//       [name]: validationResult,
-//     });
-//   };
-
-//   // const showSnackbar = (message) => {
-//   //   setSnackbarMessage(message);
-//   //   setSnackbarOpen(true);
-//   // };
-
-//   const navigat = useNavigate();
-//   const submitHandler = (e) => {
-//     e.preventDefault();
-//     console.log("Button clicked");
-
-//     props.checkValidation();
-//     setIsLoading(true);
-
-//     axios
-//       .post("http://localhost:3000/doctors", {
-//         name: formData.name,
-//         specialty: formData.specialty,
-//         location: formData.location,
-//       })
-//       .then((response) => {
-//         console.log("New Doctor created:", response.data);
-//         props.showSnackbar(
-//           `Doctor with ${JSON.stringify(response.data.id)}'s userId created...`
-//         );
-//         navigat("/");
-//       })
-
-//       .catch((err) => {
-//         console.error("Error creating post:", err);
-//         props.showSnackbar(`Error creating post: ${JSON.stringify(err)}`);
-//       })
-//       .finally(() => {
-//         setIsLoading(false);
-//         console.log(isLoading);
-//       });
-//   }; //http://localhost:3005/user
-
-//   return (
-//     <ThemeProvider theme={theme}>
-//       <Box
-//         component="form"
-//         sx={{
-//           "& > :not(style)": { m: 4 },
-//           justifyContent: "center",
-//         }}
-//         noValidate
-//         onSubmit={submitHandler}
-//         autoComplete="off"
-//       >
-//         <div>
-//           <TextField
-//             fullWidth
-//             helperText={formErrors.name ? "" : "Please enter your Name"}
-//             error={Boolean(formErrors.name)}
-//             id="outlined-basic"
-//             label="Name"
-//             name="name"
-//             value={formData.name}
-//             onChange={handleChange}
-//             required
-//           />
-//         </div>
-//         <div>
-//           <TextField
-//             helperText={"Please enter your location"}
-//             id="outlined-start-adornment"
-//             label="location"
-//             fullWidth
-//             name="location"
-//             value={formData.location}
-//             onChange={handleChange}
-//           />
-//         </div>
-
-//         <div>
-//           <TextField
-//             helperText={"Please enter your ID"}
-//             error={Boolean(formErrors.id)}
-//             id="demo-helper-txt-misaligned"
-//             label="id"
-//             fullWidth
-//             type="number"
-//             name="id"
-//             value={formData.id}
-//             onChange={handleChange}
-//             required
-//           />
-//         </div>
-//         <div>
-//           <TextField
-//             helperText={"Please enter your specialty"}
-//             error={Boolean(formErrors.specialty)}
-//             id="demo-helper-txt-misaligned"
-//             label="specialty"
-//             fullWidth
-//             name="specialty"
-//             value={formData.specialty}
-//             onChange={handleChange}
-//             required
-//           />
-//         </div>
-
-//         <Stack spacing={2} direction="row">
-//           {isLoading ? (
-//             <CircularProgress size={34}></CircularProgress>
-//           ) : (
-//             <CustomButton
-//               type="submit"
-//               variant="outlined"
-//               onClick={handleClick(TransitionLeft)}
-//             >
-//               Create Doctor
-//             </CustomButton>
-//           )}
-//         </Stack>
-//         <Snackbar
-//           TransitionComponent={transition}
-//           key={transition ? transition.name : ""}
-//           open={snackbarOpen}
-//           autoHideDuration={5000}
-//           onClose={() => setSnackbarOpen(false)}
-//           message={props.snackbarMessage}
-//         ></Snackbar>
-//       </Box>
-//     </ThemeProvider>
-//   );
-// }
