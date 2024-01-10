876bb1af58841660a555dff998f7291d0e9d3e81
SamiraMiss <samirarostami71@gmail.com>
2024-01-10 23:45:08 +0330

    Edited app whit main content in it and deleted MainContent Component.

--- src/App.js
+++ src/App.js
@@ -1,21 +1,34 @@
 import React from "react";
 import { useMode, ColorModeContext } from "./theme";
 
-import { ThemeProvider, CssBaseline } from "@mui/material";
-import MainContent from "./components/main/main";
-import SidebarMenu from "./components/global/Sidebar";
+import { Box, ThemeProvider, CssBaseline } from "@mui/material";
 
-function App() {
+import SidebarMenu from "./components/global/Sidebar";
+import { styled } from "@mui/system";
+import TopBar from "./components/global/Topbar";
+import AppRouts from "./router/AppRoutes";
+
+const StyledMainContent = styled("div")(({ isCollapsed }) => ({
+  width: "100%",
+  marginLeft: isCollapsed ? "80px" : "200px", // Adjust the width of the sidebar
+  overflowX: "hidden",
+  transition: "margin-left 0.3s ease-in-out",
+}));
+function App({ isCollapsed }) {
   const [theme, colorMode] = useMode();
 
   return (
     <ColorModeContext.Provider value={colorMode}>
       <ThemeProvider theme={theme}>
         <CssBaseline />
-
         <div className="app">
           <SidebarMenu />
-          <MainContent />
+          <StyledMainContent isCollapsed={isCollapsed}>
+            <Box className="content">
+              <TopBar />
+              <AppRouts />
+            </Box>
+          </StyledMainContent>
         </div>
       </ThemeProvider>
     </ColorModeContext.Provider>
